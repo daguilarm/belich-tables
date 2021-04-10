4b2c99a621349d82735f344b7acf023a4e0186f3
daguilarm <damian.aguilarm@gmail.com>
2021-04-10 11:54:23 +0200

    fixed styleCI

--- src/helpers.php
+++ src/helpers.php
@@ -3,8 +3,8 @@
 /**
  * Get the route base on the action.
  */
-if (!function_exists('routeAction')) {
-    function routeAction(string $root, string $action, string|object $model): string
+if (! function_exists('routeAction')) {
+    function routeAction(string $root, string $action, string | object $model): string
     {
         return route(sprintf('%s.%s', $root, $action), $model);
     }
