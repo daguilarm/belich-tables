c65c53c18abfbf06acddd9f920c85d548974128e
daguilarm <damian.aguilarm@gmail.com>
2021-04-10 11:48:20 +0200

    fixed style problems

--- src/Views/Action.php
+++ src/Views/Action.php
@@ -5,16 +5,15 @@ declare(strict_types=1);
 namespace Daguilarm\LivewireTables\Views;
 
 use Daguilarm\LivewireTables\Views\Column;
-use Illuminate\Database\Eloquent\Builder;
 
 class Action
 {
-    static public function make($model, string $routeName, ?string $view = null)
+    public static function make($model, string $routeName, ?string $view = null)
     {
         $component = $view ?? self::defaultView();
 
         return Column::make('')
-            ->format(function($model) use ($component, $routeName) {
+            ->format(function ($model) use ($component, $routeName) {
                 return view($component, compact('model', 'routeName'));
             })
             ->excludeFromExport();
