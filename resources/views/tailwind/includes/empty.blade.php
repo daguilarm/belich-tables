742ef2a6bd3fb69fd0f6709e81493a25624ee747
daguilarm <damian.aguilarm@gmail.com>
2021-04-15 20:25:44 +0200

    Fixed error message

--- resources/views/tailwind/includes/empty.blade.php
+++ resources/views/tailwind/includes/empty.blade.php
@@ -1,3 +1,5 @@
 <tr>
-    <td colspan="{{ collect($columns)->count() }}">@lang('livewire-tables::strings.no_results')</td>
+    <td colspan="{{ collect($columns)->count() + 1 }}" class="text-center p-6 bg-gradient-to-b from-white via-white to-gray-200">
+        @lang('livewire-tables::strings.no_results')
+    </td>
 </tr>
