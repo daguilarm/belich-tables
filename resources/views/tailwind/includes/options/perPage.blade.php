f1529948562997a6e060e9f6e165aa3156055fd5
daguilarm <damian.aguilarm@gmail.com>
2021-04-16 19:06:35 +0200

    Fixed css

--- resources/views/tailwind/includes/options/perPage.blade.php
+++ resources/views/tailwind/includes/options/perPage.blade.php
@@ -6,7 +6,7 @@
     <!-- Options -->
     <select
         wire:model="perPage"
-        class="block pl-3 px-5 py-1.5 text-base leading-6 border-gray-300 rounded-md shadow-md text-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
+        class="h-9 px-3 text-base leading-6 border-gray-300 rounded-md shadow-md text-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
     >
         @foreach ($perPageOptions as $option)
             <option>{{ $option }}</option>
