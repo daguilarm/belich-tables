e9124e7cb573b901734fa09b8e6ff0fb36b4a051
daguilarm <damian.aguilarm@gmail.com>
2021-04-17 18:38:26 +0200

    removed useless

--- src/Components/Traits/Loading.php
+++ src/Components/Traits/Loading.php
@@ -10,15 +10,4 @@ trait Loading
      * Whether or not to show a loading indicator when searching.
      */
     public bool $loadingIndicator = true;
-
-    /**
-     * Whether or not to disable the search bar when it is searching/loading new data.
-     */
-    public bool $disableSearchOnLoading = false;
-
-    /**
-     * When the table is loading, hide all data but the loading row
-     * Otherwise the loading row gets shown above the data (makes the page less jumpy).
-     */
-    public bool $collapseDataOnLoading = false;
 }
