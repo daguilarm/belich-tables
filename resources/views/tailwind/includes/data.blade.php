f8261a95304c9fc461520453a1d056a7cf8c1e36
daguilarm <damian.aguilarm@gmail.com>
2021-04-24 17:44:04 +0200

    dusk tags

--- resources/views/tailwind/includes/data.blade.php
+++ resources/views/tailwind/includes/data.blade.php
@@ -2,6 +2,7 @@
 @foreach($models as $model)
     <tr
         class="bg-white border-b border-gray-150"
+        dusk="row-id-{{ $model->id }}"
     >
         <!-- Table checkbox -->
         @includeWhen($showCheckboxes && !isset($headerTitle), BelichTables::include('includes.checkboxes.checkbox'))
@@ -11,15 +12,16 @@
             @if ($column->isVisible())
                 <td
                     class="{{ $column->show }} px-6 py-3 whitespace-nowrap text-sm text-gray-500"
+                    dusk="column-{{ $column->getAttribute() }}-{{ $model->id }}"
                 >
                     <!-- Render column as Boolean -->
                     @if ($column->showAsBoolean)
                         <!-- Render green -->
                         @if ($column->resolveColumn($column, $model) === true)
-                            <div class="h-4 w-4 rounded-full bg-green-400"></div>
+                            <div class="h-4 w-4 rounded-full bg-green-400" dusk="boolean-on-{{ $model->id }}"></div>
                         <!-- Render gray -->
                         @else
-                            <div class="h-4 w-4 rounded-full bg-gray-200"></div>
+                            <div class="h-4 w-4 rounded-full bg-gray-200" dusk="boolean-off-{{ $model->id }}"></div>
                         @endif
                     <!-- Render column as HTML -->
                     @elseif ($column->asHtml)
