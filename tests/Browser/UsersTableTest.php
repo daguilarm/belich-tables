63a7c11abbb6cfee1d0fc6bc71dfdf7618e9f14c
daguilarm <damian.aguilarm@gmail.com>
2021-05-08 16:51:32 +0200

    Browser tests for table

--- tests/Browser/UsersTableTest.php
+++ tests/Browser/UsersTableTest.php
@@ -4,11 +4,21 @@ declare(strict_types=1);
 
 namespace Daguilarm\BelichTables\Tests\Browser;
 
+use Daguilarm\BelichTables\Tests\App\Models\Profile;
+use Daguilarm\BelichTables\Tests\App\Models\User;
 use Daguilarm\BelichTables\Tests\BrowserTestCase as TestCase;
 
 // test --filter=UsersTableTest
 class UsersTableTest extends TestCase
 {
+    /**
+     * Setup the test environment.
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
     // test --filter=test_users_table_order_by_id
     public function test_users_table_order_by_id(): void
     {
@@ -20,15 +30,40 @@ class UsersTableTest extends TestCase
                 // Assert see in second row
                 ->assertSeeIn($this->getTablePositionSelector(2), 'Antonio Aguilar')
                 // Order the column
-                ->click('@column_id')
-                ->waitUntilMissing('#belich-tables-loading')
+                ->click('@column-id')
+                ->waitFor('@belich-tables-loading')
+                ->waitUntilMissing('@belich-tables-loading')
                 // Assert see in first row
                 ->assertSeeIn($this->getTablePositionSelector(1), 'Paula Ferrani')
                 // Assert see in last row
                 ->assertSeeIn($this->getTablePositionSelector(25), 'Carmen Porteros');
 
             // Assert total results are 25
-            $this->assertEquals('25', $this->getTotalTableRows($browser));
+            $this->assertEquals(25, $this->getTotalTableRows($browser));
+        });
+    }
+
+    // test --filter=test_users_table_search
+    public function test_users_table_search(): void
+    {
+        $this->browse(function ($browser) {
+            $browser
+                ->visit('/testing/users')
+                // Assert see in first row
+                ->type('@belich-table-search', 'Aguilar')
+                ->pause(300)
+                ->waitUntilMissing('@belich-tables-loading');
+
+            // Assert total results are 2
+            $this->assertEquals(2, $this->getTotalTableRows($browser));
+
+            // Reset search
+            $browser
+                ->click('@belich-table-clear-search')
+                ->waitUntilMissing('@belich-tables-loading');
+
+            // Assert total results are 25
+            $this->assertEquals(25, $this->getTotalTableRows($browser));
         });
     }
 }
